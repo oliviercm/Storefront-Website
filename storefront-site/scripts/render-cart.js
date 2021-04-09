01b7f3db01c5e3ac6ce474985b3239be97fc8c48
Olivier Chan <olivierchanmoy@gmail.com>
2021-04-09 14:45:52 -0700

    Handle cart quantity changes

--- storefront-site/scripts/render-cart.js
+++ storefront-site/scripts/render-cart.js
@@ -1,127 +1,107 @@
-/**
- * This script dynamically renders the cart page.
- */
+// Load test data.
+sessionStorage.setItem("cart", JSON.stringify([{
+    id: "2",
+    quantity: 1,
+    checked: true,
+    isGift: false,
+}, {
+    id: "12",
+    quantity: 2,
+    checked: true,
+    isGift: false,
+}, {
+    id: "4",
+    quantity: 3,
+    checked: false,
+    isGift: true,
+}]));
+
+// This function handles the initial render of the cart page.
 (async () => {
     try {
-        // Load test data
-        sessionStorage.setItem("cart", JSON.stringify([{
-            id: "2",
-            quantity: 1,
-            checked: true,
-            isGift: false,
-        }, {
-            id: "12",
-            quantity: 2,
-            checked: true,
-            isGift: false,
-        }, {
-            id: "4",
-            quantity: 3,
-            checked: false,
-            isGift: true,
-        }]));
-        const cart = JSON.parse(sessionStorage.getItem("cart"));
-        
-        /**
-         * Fetch and find specific product data.
-         */
-        const response = await fetch("/mock-data/products.json"); // This will be replaced with an actual API call.
-        if (response.status < 200 || response.status >= 300) {
-            throw Error(response.statusText);
-        };
-        const products = await response.json();
-
-        /**
-         * Render cart items using template.
-         */
-        const cartItemsElement = document.getElementById("cart-items");
-        const cartItemTemplate = document.getElementById("cart-item-template");
-        for (const cartItem of cart) {
-            try {
-                const item = products.find(product => {
-                    return product.id === cartItem.id;
-                });
-                if (!item) {
-                    continue;
-                };
-    
-                const cartItemElement = cartItemTemplate.content.firstElementChild.cloneNode(true);
-    
-                const cartItemCheckboxElement = cartItemElement.querySelectorAll(".cart-item-checkbox input[type=checkbox]")[0];
-                cartItemCheckboxElement.checked = cartItem.checked;
-
-                const cartItemImageElement = cartItemElement.querySelectorAll(".cart-item-image img")[0];
-                cartItemImageElement.setAttribute("src", `../images/${item.images[0]}`);
-
-                const cartItemLinkElement = cartItemElement.querySelectorAll(".cart-item-main .cart-item-link")[0];
-                cartItemLinkElement.textContent = item.name;
-                cartItemLinkElement.setAttribute("href", `./products/product.html?id=${item.id}`);
-
-                const cartItemStockElement = cartItemElement.querySelectorAll(".cart-item-main .cart-item-stock")[0];
-                const LOW_STOCK_CUTOFF = 20;
-                // If there is low stock, display a different message.
-                if (item.stock > LOW_STOCK_CUTOFF) {
-                    cartItemStockElement.textContent = `In Stock`;
-                    cartItemStockElement.style.color = `#007600`;
-                } else {
-                    cartItemStockElement.textContent = `Only ${item.stock} left in stock - order soon.`;
-                    cartItemStockElement.style.color = `#b12704`;
-                };
-
-                const cartItemIsGiftElement = cartItemElement.querySelectorAll(".cart-item-main .cart-item-gift")[0];
-                cartItemIsGiftElement.checked = cartItem.isGift;
-
-                const cartItemPriceElement = cartItemElement.querySelectorAll(".cart-item .cart-item-price")[0];
-                const originalPriceSpan = document.createElement("span");
-                originalPriceSpan.appendChild(document.createTextNode(`$${item.price.usd}`));
-                cartItemPriceElement.appendChild(originalPriceSpan);
-                // If there is a discounted price, strikethrough the original price and append the discounted price.
-                if (item.discount_price.usd) {
-                    originalPriceSpan.style.setProperty("text-decoration", "line-through");
-                    const discountedPriceSpan = document.createElement("span");
-                    discountedPriceSpan.appendChild(document.createTextNode(`$${item.discount_price.usd}`));
-                    cartItemPriceElement.appendChild(document.createTextNode("\u00A0"));
-                    cartItemPriceElement.appendChild(discountedPriceSpan);
-                };
-
-                const cartItemQuantityElement = cartItemElement.querySelectorAll(".cart-item-quantity")[0];
-                cartItemQuantityElement.value = cartItem.quantity;
-                
-                //Insert the cart item and a horizontal rule
-                cartItemsElement.appendChild(cartItemElement);
-                cartItemsElement.appendChild(document.createElement("hr"));
-            } catch(e) {
-                console.error(e);
-            };
-        };
-
-        /**
-         * Calculate and set subtotal text (total quantity, total price)
-         */
-        const {
-            totalCartPrice,
-            totalItemQuantity,
-        } = calculateTotalCartValues(products, cart);
-        const cartSubtotalItemQuantityElements = document.getElementsByClassName("total-cart-item-quantity");
-        for (const itemQuantityElement of cartSubtotalItemQuantityElements) {
-            itemQuantityElement.textContent = totalItemQuantity;
-        };
-        const cartSubtotalTotalPriceElements = document.getElementsByClassName("total-cart-subtotal");
-        for (const itemSubtotalElement of cartSubtotalTotalPriceElements) {
-            itemSubtotalElement.textContent = totalCartPrice;
-        };
-
-        /**
-         * Check the "This order contains a gift" checkbox if necessary
-         */
-        const cartContainsGiftElement = document.getElementById("cart-gift");
-        cartContainsGiftElement.checked = cart.some(cartItem => {
-            return cartItem.isGift;
-        });
+        renderCart();
     } catch(e) {
         console.error(e);
     };
 })();
+
+async function renderCart() {
+    await renderCartItems();
+    await renderTotalCartValues();
+};
+
+async function renderCartItems() {
+    const cart = getCart();
+    const products = await getProducts();
+
+    // Render cart items using template.
+    const cartItemsElement = document.getElementById("cart-items");
+    const cartItemTemplate = document.getElementById("cart-item-template");
+    for (const cartItem of cart) {
+        try {
+            const item = products.find(product => {
+                return product.id === cartItem.id;
+            });
+            if (!item) {
+                continue;
+            };
+
+            const cartItemElement = cartItemTemplate.content.firstElementChild.cloneNode(true);
+            cartItemElement.setAttribute("data-product-id", cartItem.id);
+
+            const cartItemCheckboxElement = cartItemElement.querySelectorAll(".cart-item-checkbox input[type=checkbox]")[0];
+            cartItemCheckboxElement.checked = cartItem.checked;
+
+            const cartItemImageElement = cartItemElement.querySelectorAll(".cart-item-image img")[0];
+            cartItemImageElement.setAttribute("src", `../images/${item.images[0]}`);
+
+            const cartItemLinkElement = cartItemElement.querySelectorAll(".cart-item-main .cart-item-link")[0];
+            cartItemLinkElement.textContent = item.name;
+            cartItemLinkElement.setAttribute("href", `./products/product.html?id=${item.id}`);
+
+            const cartItemStockElement = cartItemElement.querySelectorAll(".cart-item-main .cart-item-stock")[0];
+            const LOW_STOCK_CUTOFF = 20;
+            // If there is low stock, display a different message.
+            if (item.stock > LOW_STOCK_CUTOFF) {
+                cartItemStockElement.textContent = `In Stock`;
+                cartItemStockElement.style.color = `#007600`;
+            } else {
+                cartItemStockElement.textContent = `Only ${item.stock} left in stock - order soon.`;
+                cartItemStockElement.style.color = `#b12704`;
+            };
+
+            const cartItemIsGiftElement = cartItemElement.querySelectorAll(".cart-item-main .cart-item-gift")[0];
+            cartItemIsGiftElement.checked = cartItem.isGift;
+
+            const cartItemPriceElement = cartItemElement.querySelectorAll(".cart-item .cart-item-price")[0];
+            const originalPriceSpan = document.createElement("span");
+            originalPriceSpan.classList.add("cart-item-price-original");
+            originalPriceSpan.setAttribute("data-product-id", cartItem.id);
+            originalPriceSpan.appendChild(document.createTextNode(`$${(item.price.usd * cartItem.quantity).toFixed(2)}`));
+            cartItemPriceElement.appendChild(originalPriceSpan);
+            // If there is a discounted price, strikethrough the original price and append the discounted price.
+            if (item.discount_price.usd) {
+                originalPriceSpan.style.setProperty("text-decoration", "line-through");
+                const discountedPriceSpan = document.createElement("span");
+                discountedPriceSpan.classList.add("cart-item-price-discount");
+                discountedPriceSpan.setAttribute("data-product-id", cartItem.id);
+                discountedPriceSpan.appendChild(document.createTextNode(`$${(item.discount_price.usd * cartItem.quantity).toFixed(2)}`));
+                cartItemPriceElement.appendChild(document.createTextNode("\u00A0"));
+                cartItemPriceElement.appendChild(discountedPriceSpan);
+            };
+
+            const cartItemQuantityElement = cartItemElement.querySelectorAll(".cart-item-quantity")[0];
+            cartItemQuantityElement.value = cartItem.quantity;
+            cartItemQuantityElement.setAttribute("data-product-id", cartItem.id);
+            
+            // Insert the cart item and a horizontal rule
+            cartItemsElement.appendChild(cartItemElement);
+            cartItemsElement.appendChild(document.createElement("hr")).classList.add("cart-item-hr");
+        } catch(e) {
+            console.error(e);
+        };
+    };
+};
 
 function calculateTotalCartValues(products, cart) {
     let totalCartPrice = 0;
@@ -139,4 +119,92 @@ function calculateTotalCartValues(products, cart) {
         totalCartPrice,
         totalItemQuantity,
     };
+};
+
+async function renderTotalCartValues() {
+    const cart = getCart();
+    const products = await getProducts();
+
+    // Calculate and set subtotal text (total quantity, total price)
+    const {
+        totalCartPrice,
+        totalItemQuantity,
+    } = calculateTotalCartValues(products, cart);
+    const cartSubtotalItemQuantityElements = document.getElementsByClassName("total-cart-item-quantity");
+    for (const itemQuantityElement of cartSubtotalItemQuantityElements) {
+        itemQuantityElement.textContent = totalItemQuantity;
+    };
+    const cartSubtotalTotalPriceElements = document.getElementsByClassName("total-cart-subtotal");
+    for (const itemSubtotalElement of cartSubtotalTotalPriceElements) {
+        itemSubtotalElement.textContent = totalCartPrice.toFixed(2);
+    };
+
+    // Check the "This order contains a gift" checkbox if necessary
+    const cartContainsGiftElement = document.getElementById("cart-gift");
+    cartContainsGiftElement.checked = cart.some(cartItem => {
+        return cartItem.isGift;
+    });
+};
+
+function handleQuantityChange(event) {
+    const cart = getCart();
+    const changedCartItem = cart.find(cartItem => {
+        return cartItem.id === event.target.dataset.productId;
+    });
+    changedCartItem.quantity = Number(event.target.value);
+    setCart(cart);
+    updateCartItemQuantity(changedCartItem.id);
+    renderTotalCartValues();
+};
+
+async function updateCartItemQuantity(productId) {
+    const product = await getProductById(productId);
+    const cartItem = getCartItemById(productId);
+
+    const cartItemOriginalPriceElement = Array.from(document.getElementsByClassName("cart-item-price-original")).find(element => {
+        return element.dataset.productId === productId;
+    });
+    cartItemOriginalPriceElement.textContent = `$${(product.price.usd * cartItem.quantity).toFixed(2)}`;
+    
+    const cartItemDiscountPriceElement = Array.from(document.getElementsByClassName("cart-item-price-discount")).find(element => {
+        return element.dataset.productId === productId;
+    });
+    if (cartItemDiscountPriceElement) {
+        cartItemDiscountPriceElement.textContent = `$${(product.discount_price.usd * cartItem.quantity).toFixed(2)}`;
+    };
+
+    // const cartItemQuantityElement = cartItemElement.querySelectorAll(".cart-item-quantity")[0];
+    // cartItemQuantityElement.value = cartItem.quantity;
+    // cartItemQuantityElement.setAttribute("data-product-id", cartItem.id);
+};
+
+async function getProducts() {
+    const response = await fetch("/mock-data/products.json"); // This will be replaced with an actual API call.
+    if (response.status < 200 || response.status >= 300) {
+        throw Error(response.statusText);
+    };
+    const products = await response.json();
+    return products;
+};
+
+async function getProductById(id) {
+    const products = await getProducts();
+    return products.find(product => {
+        return product.id === id;
+    });
+};
+
+function getCart() {
+    return JSON.parse(sessionStorage.getItem("cart"));
+};
+
+function setCart(cart) {
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+};
+
+function getCartItemById(id) {
+    const cart = getCart();
+    return cart.find(cartItem => {
+        return cartItem.id === id;
+    });
 };
