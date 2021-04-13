f8b21808982ef708a3460f056bd2ca2de40c2a09
Olivier Chan <olivierchanmoy@gmail.com>
2021-04-12 19:03:23 -0700

    Clean up homepage render script

--- storefront-site/scripts/render-home.js
+++ storefront-site/scripts/render-home.js
@@ -1,37 +1,22 @@
-
-//Pulls data from JSON
-async function getProducts() {
-    const response = await fetch("/mock-data/products.json"); // This will be replaced with an actual API call.  t   `
-    if (response.status < 200 || response.status >= 300) {     
-        throw Error(response.statusText);
-    };
-    const products = await response.json();
-    return products;
-};
+import {
+    getProducts,
+} from "./util-products.js";
 
 const productData = await getProducts();
-
-//gets the product-grid
-const productGrid = document.querySelector(".inner-grid-product-listings")
-
-//gets the template 
+const productGrid = document.querySelector(".inner-grid-product-listings");
 const productTemplate = document.getElementById("product-template");
 
-//loops through to create copies and append to the DOM
+// Loops all products to create product listings and append to the product grid
 for (const product of productData) {
+    const productElement = productTemplate.content.firstElementChild.cloneNode(true);
+    productElement.setAttribute("product-id", product.id);
 
-        const productElement = productTemplate.content.firstElementChild.cloneNode(true);
-        productElement.setAttribute("product-id", product.id);
-        console.log(productElement);
+    const productImageElement = productElement.querySelector("img");
+    productImageElement.setAttribute("src", `./images/${product.images[0]}`);
+    productImageElement.setAttribute("alt", product.name);
 
-        const productImageElement = productElement.querySelector("img");
-        productImageElement.setAttribute("src", `./images/${product.images[0]}`);
-        console.log(productImageElement); 
+    const productLinkElement = productElement.querySelector("a");
+    productLinkElement.setAttribute("href", `./html/products/product.html?id=${product.id}`);
 
-        const productLinkElement = productElement.querySelector("a");
-        productLinkElement.setAttribute("href", `./html/products/product.html?id=${product.id}`);
-        console.log(productLinkElement);  
-
-        productGrid.appendChild(productElement);
-
-        };
+    productGrid.appendChild(productElement);
+};
