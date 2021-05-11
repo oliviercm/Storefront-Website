37a85ce18c9e48c67a7e88458b4550b4913a446c
Olivier Chan <olivierchanmoy@gmail.com>
2021-05-10 21:21:33 -0700

    Set default PDO fetch mode

--- storefront-site/php/mysql.php
+++ storefront-site/php/mysql.php
@@ -13,6 +13,7 @@ class MySQL {
 
         $this->conn = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
 
     public function getProductById(int $id) {
