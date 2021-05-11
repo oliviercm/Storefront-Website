5be1c025c0593f31d7b8bba4b0bf834bb33735d7
Olivier Chan <olivierchanmoy@gmail.com>
2021-05-10 23:29:39 -0700

    Update homepage rendering to new schema

--- storefront-site/scripts/render-home.js
+++ storefront-site/scripts/render-home.js
@@ -16,7 +16,7 @@ import {
         productElement.setAttribute("product-id", product.id);
 
         const productImageElement = productElement.querySelector("img");
-        productImageElement.setAttribute("src", `./images/${product.images[0]}`);
+        productImageElement.setAttribute("src", `./images/${product.image}`);
 
         const productLinkElement = productElement.querySelector("a");
         productLinkElement.setAttribute("href", `./html/products/product.php?id=${product.id}`);
@@ -27,25 +27,25 @@ import {
     //Display popular product
     const randomPopular = productData[Math.floor(Math.random() * productData.length)];
     const popularProductImage = document.getElementById("random-popular-product");
-    popularProductImage.setAttribute("src", `./images/${randomPopular.images}`);
+    popularProductImage.setAttribute("src", `./images/${randomPopular.image}`);
     const popularProductLink = document.getElementById("popular-product-link");
     popularProductLink.setAttribute("href", `./html/products/product.php?id=${randomPopular.id}`);
     const seePopularProducts = document.getElementById("see-popular-products");
     seePopularProducts.textContent = "See More";
     seePopularProducts.setAttribute("href", `./html/products/product.php?id=${randomPopular.id}`);
 
-    //Display product under $20 
+    //Display product under $20
     const cheaperProducts = [];
 
     for (const cheapProduct of productData) {
-        if (cheapProduct.price.usd < 20) {
+        if (cheapProduct.price < 2000) {
             cheaperProducts.push(cheapProduct);
         }
     }
 
     const cheapItem = cheaperProducts[Math.floor(Math.random() * cheaperProducts.length)];
     const cheapProductImage = document.getElementById("random-cheap-product");
-    cheapProductImage.setAttribute("src", `./images/${cheapItem.images}`);
+    cheapProductImage.setAttribute("src", `./images/${cheapItem.image}`);
     const cheapProductLink = document.getElementById("cheap-product-link");
     cheapProductLink.setAttribute("href", `./html/products/product.php?id=${cheapItem.id}`);
     const seeCheapProducts = document.getElementById("see-cheaper-products");
@@ -56,7 +56,7 @@ import {
     const saleProducts = [];
 
     for (const discountedProduct of productData) {
-        if (discountedProduct.discount_price.usd) {
+        if (discountedProduct.discount_price) {
             saleProducts.push(discountedProduct);
         }
     }
@@ -64,7 +64,7 @@ import {
     const saleItem = saleProducts[Math.floor(Math.random() * saleProducts.length)];
 
     const saleProductImage = document.getElementById("random-sale-product");
-    saleProductImage.setAttribute("src", `./images/${saleItem.images}`);
+    saleProductImage.setAttribute("src", `./images/${saleItem.image}`);
     const saleProductLink = document.getElementById("sale-product-link");
     saleProductLink.setAttribute("href", `./html/products/product.php?id=${saleItem.id}`);
     const seeSaleProducts = document.getElementById("see-sale-products");
@@ -79,7 +79,7 @@ import {
     const highRating = sorted[0];
 
     const highRatedImage = document.getElementById("high-rated-product");
-    highRatedImage.setAttribute("src", `./images/${highRating.images}`);
+    highRatedImage.setAttribute("src", `./images/${highRating.image}`);
     const highRatedLink = document.getElementById("high-rated-product-link");
     highRatedLink.setAttribute("href", `./html/products/product.php?id=${highRating.id}`);
     const highRatedProducts = document.getElementById("see-high-rated-products");
