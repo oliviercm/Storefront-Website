6c68165630ed1ba3d576284027a3d13a868b2bf2
Olivier Chan <olivierchanmoy@gmail.com>
2021-05-11 18:27:09 -0700

    Add JWT verification and decoding

--- storefront-site/php/jwt.php
+++ storefront-site/php/jwt.php
@@ -1,19 +1,23 @@
 <?php
 namespace JWT;
 
-function encode($payload) {
+function _sign($payload) {
     $secret = getenv("JWT_SECRET");
+    if (empty($secret)) {
+        throw new \Exception("No secret configured.");
+    }
+    return hash_hmac("sha256", $payload, $secret, true);
+}
+
+function encode($payload) {
     $expire_time = getenv("JWT_EXPIRE_TIME");
+    if (empty($expire_time)) {
+        throw new \Exception("No JWT expire time configured.");
+    }
     $payload = array_merge($payload, [
         "iat" => time(),
         "exp" => time() + $expire_time
     ]);
-    if (empty($secret)) {
-        throw new \Exception("No secret configured.");
-    }
-    if (empty($expire_time)) {
-        throw new \Exception("No JWT expire time configured.");
-    }
 
     $header = json_encode([
         "typ" => "JWT",
@@ -22,10 +26,28 @@ function encode($payload) {
     $encoded_payload = json_encode($payload);
     $base64UrlHeader = str_replace(["+", "/", "="], ["-", "_", ""], base64_encode($header));
     $base64UrlPayload = str_replace(["+", "/", "="], ["-", "_", ""], base64_encode($encoded_payload));
-    $signature = hash_hmac("sha256", $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
+    $signature = _sign($base64UrlHeader . "." . $base64UrlPayload);
     $base64UrlSignature = str_replace(["+", "/", "="], ["-", "_", ""], base64_encode($signature));
     $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
 
     return $jwt;
+}
+
+function verify($jwt) {
+    [$headerEncoded, $payloadEncoded, $signatureEncoded] = explode(".", $jwt);
+    $signature = base64_decode($signatureEncoded);
+    $hash = _sign(implode(".", [$headerEncoded, $payloadEncoded]));
+
+    return hash_equals($signature, $hash);
+}
+
+function decode($jwt) {
+    if (verify($jwt)) {
+        [$headerEncoded, $payloadEncoded, $signatureEncoded] = explode(".", $jwt);
+
+        return json_decode(base64_decode($payloadEncoded));
+    } else {
+        throw new \Exception("Incorrect JWT signature.");
+    }
 }
 ?>
