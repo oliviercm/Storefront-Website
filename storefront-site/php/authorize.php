09eb4ab2430f657b60a5b82e0eb1c85b64938b46
Olivier Chan <olivierchanmoy@gmail.com>
2021-05-11 22:09:14 -0700

    Add authorization helper

---
+++ storefront-site/php/authorize.php
@@ -0,0 +1,27 @@
+<?php
+namespace Authorization;
+require_once $_SERVER["DOCUMENT_ROOT"] . "/php/jwt.php";
+
+function getRequestAuthorizedUser() {
+    $accessToken = $_COOKIE["access_token"];
+    if (empty($accessToken)) {
+        return false;
+    }
+    $decoded = \JWT\decode($accessToken);
+    if (empty($decoded)) {
+        return false;
+    }
+    $accessTokenCsrfToken = $decoded->{"csrf-token"};
+    if (empty($accessTokenCsrfToken)) {
+        return false;
+    }
+    $requestHeaders = getallheaders();
+    if (empty($requestHeaders["X-CSRF-TOKEN"])) {
+        return false;
+    }
+    if ($accessTokenCsrfToken !== $requestHeaders["X-CSRF-TOKEN"]) {
+        return false;
+    }
+    return $decoded->user_id;
+};
+?>
