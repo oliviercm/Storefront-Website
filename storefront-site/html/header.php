9f48a0ee412c04144714f7f7d38128b02505df02
Olivier Chan <olivierchanmoy@gmail.com>
2021-05-11 23:04:06 -0700

    Dynamic header

--- storefront-site/html/header.php
+++ storefront-site/html/header.php
@@ -10,18 +10,18 @@
     <nav class="header-nav">
         <a href="/html/cart.php" class="header-nav-link"><span class="material-icons">shopping_cart</span> Cart
             (<span class="cart-total">0</span>)</a>
-        <a href="/html/user-preferences.php" class="header-nav-link"><span class="material-icons">settings</span>
-            Preferences</a>
         <div class="header-login-dropdown">
-            <a href="/html/login.php" class="header-nav-link">Sign In ▼</a>
+            <a id="account-dropdown" class="header-nav-link" onclick="handleAccountDropdownButton()">Sign In ▼</a>
             <nav class="header-login-dropdown-content">
                 <div>
                     <div class="header-login-dropdown-flex">
-                        <a href="/html/login.php">Sign In <span class="material-icons"
+                        <a id="login-button" href="/html/login.php">Sign In <span class="material-icons"
                                 style="font-size: 16px;">login</span></a>
-                        <a href="/html/register.php">Register <span class="material-icons"
+                        <a id="register-button" href="/html/register.php">Register <span class="material-icons"
                                 style="font-size: 16px;">person_add</span></a>
-                        <a href="/index.php">Log Out <span class="material-icons"
+                        <a id="preferences-button" href="/html/user-preferences.php">Preferences <span class="material-icons"
+                            style="font-size: 16px;">settings</span></a>
+                        <a id="logout-button" onclick="logoutUser()">Log Out <span class="material-icons"
                                 style="font-size: 16px;">logout</span></a>
                     </div>
                 </div>
@@ -30,3 +30,31 @@
     </nav>
 </header>
 <hr style="margin: 0; padding: 0; border: 0; height: 24px; box-shadow: inset 0 24px 24px -24px rgba(0, 0, 0, 0.5);">
+<script>
+    if (localStorage.getItem("csrf-token")) {
+        document.getElementById("account-dropdown").textContent = "Account ▼";
+        document.getElementById("login-button").style.display = "none";
+        document.getElementById("register-button").style.display = "none";
+        document.getElementById("preferences-button").style.display = "block";
+        document.getElementById("logout-button").style.display = "block";
+    } else {
+        document.getElementById("account-dropdown").textContent = "Sign In ▼";
+        document.getElementById("login-button").style.display = "block";
+        document.getElementById("register-button").style.display = "block";
+        document.getElementById("preferences-button").style.display = "none";
+        document.getElementById("logout-button").style.display = "none";
+    };
+
+    function handleAccountDropdownButton() {
+        if (localStorage.getItem("csrf-token")) {
+            logoutUser();
+        } else {
+            window.location.href = "/html/login.php";
+        };
+    };
+
+    function logoutUser() {
+        localStorage.removeItem("csrf-token");
+        window.location.replace("/index.php");
+    };
+</script>
